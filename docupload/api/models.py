878cfa60d9f87701ad137c9cecebe91d7902d302
Nikolas Tay <niktay94@gmail.com>
2018-12-02 01:13:01 +0800

    Create Django database model (#9)
    * Upgrade to Django 2.0
    
    * Add database model

--- docupload/api/models.py
+++ docupload/api/models.py
@@ -1,3 +1,32 @@
-from django.db import models  # noqa: F401
+from django.db import models
 
-# Create your models here.
+
+class Trackable(models.Model):
+    total_docs = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class FileType(Trackable):
+    name = models.CharField(max_length=255)
+
+
+class Confidentiality(Trackable):
+    name = models.CharField(max_length=255)
+
+
+class Language(Trackable):
+    name = models.CharField(max_length=255)
+    short = models.CharField(max_length=64)
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=255)
+    size = models.PositiveIntegerField()
+    doc_file = models.FileField(storage='/uploads')
+    filetype = models.ForeignKey(FileType, on_delete=models.PROTECT)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    confidentiality = models.ForeignKey(
+        Confidentiality, on_delete=models.PROTECT,
+    )
